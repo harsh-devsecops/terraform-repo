185534f10fbb3346e899ba303dcbeaa85b71980b
gith <149611366+git01h@users.noreply.github.com>
2023-11-08 16:55:21 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "Harsh_terraform"
-    storage_account_name  = "dummyterraform1"
-    container_name        = "terrraformstate"
-    key                   = "sample/vm"
-  }
-}
