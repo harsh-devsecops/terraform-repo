903744e61def589b1eec608d2f0da4d7352a8dfd
gith <149611366+git01h@users.noreply.github.com>
2023-11-21 20:24:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,53 +27,53 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = var.subnet_prefix
 }
 
-resource "azurerm_network_interface" "interface" {
-  name                = var.azurerm_interface
-  location            = azurerm_resource_group.az.location
-  resource_group_name = azurerm_resource_group.az.name
+#resource "azurerm_network_interface" "interface" {
+ # name                = var.azurerm_interface
+ # location            = azurerm_resource_group.az.location
+ # resource_group_name = azurerm_resource_group.az.name
 
-  ip_configuration {
-    name                          = var.ip_configuration
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = var.private_ip
-  }
-}
+ # ip_configuration {
+ #   name                          = var.ip_configuration
+ #   subnet_id                     = azurerm_subnet.subnet.id
+ #   private_ip_address_allocation = var.private_ip
+#  }
+#}
 
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.az.name
-  location            = azurerm_resource_group.az.location
-  size                = var.vm_size
-  admin_username      = var.vm_admin_username
-  admin_password      = var.vm_admin_password
-  network_interface_ids = [
-    azurerm_network_interface.interface.id,
-  ]
+#resource "azurerm_windows_virtual_machine" "vm" {
+#  name                = var.vm_name
+#  resource_group_name = azurerm_resource_group.az.name
+#  location            = azurerm_resource_group.az.location
+#  size                = var.vm_size
+#  admin_username      = var.vm_admin_username
+#  admin_password      = var.vm_admin_password
+#  network_interface_ids = [
+#    azurerm_network_interface.interface.id,
+ # ]
 
-  os_disk {
-    caching              = var.os_caching
-    storage_account_type = var.os_account
-  }
+ # os_disk {
+ #   caching              = var.os_caching
+ #   storage_account_type = var.os_account
+#  }
 
-  source_image_reference {
-    publisher = var.si_publisher
-    offer     = var.si_offer
-    sku       = var.si_sku
-    version   = var.si_version
-  }
-}
+  #source_image_reference {
+    #publisher = var.si_publisher
+    #offer     = var.si_offer
+   # sku       = var.si_sku
+  #  version   = var.si_version
+ # }
+#}
 
-resource "azurerm_storage_account" "storage" {
-  name                     = var.sa_name
-  resource_group_name      = azurerm_resource_group.az.name
-  location                 = azurerm_resource_group.az.location
-  account_tier             = var.sa_account_tier
-  account_replication_type = var.account_replication_type
+#resource "azurerm_storage_account" "storage" {
+ # name                     = var.sa_name
+#  resource_group_name      = azurerm_resource_group.az.name
+#  location                 = azurerm_resource_group.az.location
+  #account_tier             = var.sa_account_tier
+ # account_replication_type = var.account_replication_type
 
-  tags = {
-    environment = var.environment
-  }
-}
+  #tags = {
+  #  environment = var.environment
+ # }
+#}
 
 
 # # Create a virtual network within the resource group
